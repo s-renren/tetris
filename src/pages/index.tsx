30e91dcf756792d2bf993e851076b5bde302b7b4
s-renren <sakurazawaren@gmail.com>
2024-06-09 23:31:20 +0900

    ボードを作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,18 @@
+import { useState } from 'react';
 import styles from './index.module.css';
 
 const Home = () => {
-  return <div className={styles.container}>tetris!</div>;
+  const [board, setBoard] = useState([...Array(20)].map(() => [...Array(10)].map(() => 0)));
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.backBoard}>
+        {board.map((row, y) =>
+          row.map((number, x) => <div className={styles.cell} key={`${x}-${y}`} />),
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
