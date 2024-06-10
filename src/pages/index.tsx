92728c2cb203ebc39ff0c00bd876b0c5ec253d4f
s-renren <sakurazawaren@gmail.com>
2024-06-10 11:57:00 +0900

    oミノとミノのcssを追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,28 @@ import { useState } from 'react';
 import styles from './index.module.css';
 
 const Home = () => {
-  const [board, setBoard] = useState([...Array(20)].map(() => [...Array(10)].map(() => 0)));
+  const [board, setBoard] = useState([...Array(20)].map(() => [...Array(10)].map(() => 3)));
 
   return (
     <div className={styles.container}>
       <div className={styles.backBoard}>
         {board.map((row, y) =>
-          row.map((number, x) => <div className={number === 0 ? styles.cell : number === 1 ? styles.iStyle : ''} key={`${x}-${y}`} />),
+          row.map((number, x) => (
+            <div
+              className={
+                number === 0
+                  ? styles.cell
+                  : number === 1
+                    ? styles.iMino
+                    : number === 2
+                      ? styles.oMino
+                      : number === 3
+                        ? styles.sMino
+                        : ''
+              }
+              key={`${x}-${y}`}
+            />
+          )),
         )}
       </div>
     </div>
