9466b02a915bfd425ec9a2677420b77650032830
s-renren <sakurazawaren@gmail.com>
2024-06-26 16:20:46 +0900

    エラーを削除した

--- src/components/backBoard/backBoard.tsx
+++ src/components/backBoard/backBoard.tsx
@@ -1,36 +1,33 @@
-import styles from "./backBoard.module.css"
+import styles from './backBoard.module.css';
 type Props = {
-  board: number[][]
-}
+  board: number[][];
+};
 
-export const BackBoard = ({
-  board,
-
-}: Props) => (
+export const BackBoard = ({ board }: Props) => (
   <div className={styles.backBoard}>
-          {board.map((row, y) =>
-            row.map((number, x) => (
-              <div
-                className={
-                  number === 0
-                    ? styles.cell
-                    : number === 1
-                      ? styles.iMino
-                      : number === 2
-                        ? styles.oMino
-                        : number === 3
-                          ? styles.sMino
-                          : number === 4
-                            ? styles.zMino
-                            : number === 5
-                              ? styles.jMino
-                              : number === 6
-                                ? styles.lMino
-                                : styles.tMino
-                }
-                key={`${x}-${y}`}
-              />
-            )),
-          )}
-        </div>
-)
+    {board.map((row, y) =>
+      row.map((number, x) => (
+        <div
+          className={
+            number === 0
+              ? styles.cell
+              : number === 1
+                ? styles.iMino
+                : number === 2
+                  ? styles.oMino
+                  : number === 3
+                    ? styles.sMino
+                    : number === 4
+                      ? styles.zMino
+                      : number === 5
+                        ? styles.jMino
+                        : number === 6
+                          ? styles.lMino
+                          : styles.tMino
+          }
+          key={`${x}-${y}`}
+        />
+      )),
+    )}
+  </div>
+);
